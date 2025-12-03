6a5c8c8fd62b7b31678eeb890a953a5e73428fb6
lunrnightngale <media.djohan@gmail.com>
2025-12-03 20:07:30 +0700

    Update navigation links in script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function code() {
+    document.getElementById('links').innerHTML = '\[ <a href="index.html">> Home</a> | <a href="about.html">> About</a> | <a href="text.html">> Information Booklet</a> | <a href="game.html">> Game</a> | <a href="ppt.html">> Powerpoint</a> | <a href="poster.html">> Posters</a> \]';
+};
+
+window.onload = code;
