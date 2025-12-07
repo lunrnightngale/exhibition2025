c761ca80f87a0419e6770aa2dae6923b9c053223
lunrnightngale <media.djohan@gmail.com>
2025-12-07 20:09:34 +0700

    Refactor sidebar navigation functionality

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ function code() {
     document.getElementById('links').innerHTML = '\[ <a href="index.html">> Home</a> | <a href="about.html">> About</a> | <a href="text.html">> Information Booklet</a> | <a href="game.html">> Game</a> | <a href="ppt.html">> Powerpoint</a> | <a href="poster.html">> Posters</a> \]';
 };
 
-window.onload = code;
-function openNav() {
-  document.getElementById("mySidebar").style.width = "30";
-  document.getElementById("main").style.marginLeft = "30";
-}
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+{
+  let sideBar = document.querySelector('.side-bar');
+  let arrowCollapse = document.querySelector('#logo-name__icon');
+  sideBar.onclick = () => {
+    sideBar.classList.toggle('collapse');
+    arrowCollapse.classList.toggle('collapse');
+    if (arrowCollapse.classList.contains('collapse')) {
+      arrowCollapse.classList =
+        'bx bx-arrow-from-left logo-name__icon collapse';
+    } else {
+      arrowCollapse.classList = 'bx bx-arrow-from-right logo-name__icon';
+    }
+  };
 }
