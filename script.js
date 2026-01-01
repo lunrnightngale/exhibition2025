d49d17c3dae625c82c5907a0335dc4ae6688d6f4
lunrnightngale <media.djohan@gmail.com>
2026-01-01 10:26:10 +0800

    Remove sidebar toggle functionality
    Removed sidebar toggle functionality from script.js.

--- script.js
+++ script.js
@@ -2,17 +2,3 @@ function code() {
     document.getElementById('links').innerHTML = '\[ <a href="index.html">> Home</a> | <a href="about.html">> About</a> | <a href="text.html">> Information Booklet</a> | <a href="game.html">> Game</a> | <a href="ppt.html">> Powerpoint</a> | <a href="poster.html">> Posters</a> \]';
 };
 
-{
-  let sideBar = document.querySelector('.side-bar');
-  let arrowCollapse = document.querySelector('#logo-name__icon');
-  sideBar.onclick = () => {
-    sideBar.classList.toggle('collapse');
-    arrowCollapse.classList.toggle('collapse');
-    if (arrowCollapse.classList.contains('collapse')) {
-      arrowCollapse.classList =
-        'bx bx-arrow-from-left logo-name__icon collapse';
-    } else {
-      arrowCollapse.classList = 'bx bx-arrow-from-right logo-name__icon';
-    }
-  };
-}
